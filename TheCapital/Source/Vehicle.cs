b80cabbe950817e9db3557771451e5525447c636
Olivier Leduc <account@leduc.cc>
2018-09-07 16:53:07 -0400

    Fix defs

--- TheCapital/Source/Vehicle.cs
+++ TheCapital/Source/Vehicle.cs
@@ -1,18 +1,10 @@
 ﻿using RimWorld;
-using TheCapital.Trackers;
 using Verse;
 
 namespace TheCapital
 {
-    public class Vehicle : Actor
+    public class Vehicle : Pawn
     {
-        public VehicleStoryTracker story;
 
-        public bool isDestroyed;
-        
-        public Vehicle()
-        {
-            
-        }
     }
 }
